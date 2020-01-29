4f7bf3cd4be83b754f4f96f7e8dc0d14fa90ccd9
zomblzum <46081768+zomblzum@users.noreply.github.com>
2020-01-29 05:35:56 +0500

    change if order and delete redurant check

--- task_1/sum_searcher.py
+++ task_1/sum_searcher.py
@@ -6,10 +6,7 @@ def list_contain_equal_sum(array, result):
     hash_array = {}
 
     for i in array:
-        if hash_array.get(i) and i + i == result:
-            return True 
-
-        hash_array[result - i] = i
-
-        if hash_array.get(i) and hash_array.get(i) != i:
+        if hash_array.get(i):
             return True
+        
+        hash_array[i] = result - i
